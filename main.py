c2263428eb799a3dc7e106f0c0156b9ed8725c70
Eng/Ibrahim Shahin <ibrahimsaaed38@gmail.com>
2023-12-20 21:22:30 +0200

    final model

--- main.py
+++ main.py
@@ -17,28 +17,55 @@ encoder.fit(Y)
 haarcascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 model = pickle.load(open("svm_model_160x160.pkl", 'rb'))
 
-cap = cv.VideoCapture(0)
-# WHILE LOOP
+if __name__ == "__main__":
+    cap = cv.VideoCapture(0)
+    # WHILE LOOP
 
-while cap.isOpened():
-    _, frame = cap.read()
+    while cap.isOpened():
+        _, frame = cap.read()
+        frame = cv.flip(frame, 1)
+        rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = haarcascade.detectMultiScale(gray_img, 1.3, 5)
+        for x,y,w,h in faces:
+            img = rgb_img[y:y+h, x:x+w]
+            img = cv.resize(img, (160,160)) # 1x160x160x3
+            img = np.expand_dims(img,axis=0)
+            ypred = facenet.embeddings(img)
+            face_name = model.predict(ypred)
+            final_name = encoder.inverse_transform(face_name)[0]
+            cv.rectangle(frame, (x,y), (x+w,y+h), (255,0,255), 10)
+            cv.putText(frame, str(final_name), (x,y-10), cv.FONT_HERSHEY_SIMPLEX,
+                       1, (0,0,255), 3, cv.LINE_AA)
+
+        cv.imshow("Face Recognition:", frame)
+        # if cv.waitKey(1) & ord('q') ==27:
+        key = cv.waitKey(1)
+        if key == 27: #ord('q'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows
+
+def verify(frame,draw = True):
     rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = haarcascade.detectMultiScale(gray_img, 1.3, 5)
-    for x,y,w,h in faces:
-        img = rgb_img[y:y+h, x:x+w]
-        img = cv.resize(img, (160,160)) # 1x160x160x3
-        img = np.expand_dims(img,axis=0)
+
+    for x, y, w, h in faces:
+        img = rgb_img[y:y + h, x:x + w]
+        img = cv.resize(img, (160, 160))  # 1x160x160x3
+        img = np.expand_dims(img, axis=0)
         ypred = facenet.embeddings(img)
         face_name = model.predict(ypred)
         final_name = encoder.inverse_transform(face_name)[0]
-        cv.rectangle(frame, (x,y), (x+w,y+h), (255,0,255), 10)
-        cv.putText(frame, str(final_name), (x,y-10), cv.FONT_HERSHEY_SIMPLEX,
-                   1, (0,0,255), 3, cv.LINE_AA)
+        if draw:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 10)
+            cv.putText(frame, str(final_name), (x, y - 10), cv.FONT_HERSHEY_SIMPLEX,
+                       1, (0, 0, 255), 3, cv.LINE_AA)
 
-    cv.imshow("Face Recognition:", frame)
-    if cv.waitKey(1) & ord('q') ==27:
-        break
+        if final_name == "others":
+            return False
+        else :
+            return True
 
-cap.release()
-cv.destroyAllWindows
